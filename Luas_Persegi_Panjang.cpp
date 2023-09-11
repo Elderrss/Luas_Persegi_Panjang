a54fedc0ef919071e03aa969e1401219f94627a3
Elderrss <144640867+Elderrss@users.noreply.github.com>
2023-09-11 09:29:36 +0700

    Create Luas_Persegi_Panjang.cpp

---
+++ Luas_Persegi_Panjang.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int p, l, L;
+    cout<<"MENGHITUNG LUAS PERSEGI PANJANG \n";
+    cout<<"<=============================> \n";
+    cout<<"masukan panjang:";
+    cin>>p;
+    cout<<"masukan lebar:";
+    cin>>l;
+    L=p*l;
+    cout<<"hasil adalah:"<<L<<endl;
+    return 0;}
